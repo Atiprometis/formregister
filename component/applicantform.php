553080163ceb332e7f8c4b3d45c27c7160137db6
DESKTOP-FO9RCJD\FANG <fang.live-@msn.com>
2021-03-16 16:44:27 +0700

    addd

---
+++ component/applicantform.php
@@ -0,0 +1,42 @@
+<div class="col-12">
+    <h1 class=" col-12 text-center pt-3 ">ใบสมัครงาน</h1>
+    <div class=" col-12 ">
+        <div class=" col-12 d-flex justify-content-between flex-wrap">
+            <div class="col-6 form-group w-100  p-0 pr-3  m-0">
+                <label for=""></label>
+                <small id="helpId" class="form-text text-muted">ชื่อ</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-6 form-group w-100 p-0 m-0">
+                <label for=""></label>
+                <small id="helpId" class="form-text text-muted">นามสกุล</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class=" col-6 form-group p-0   pr-3 m-0">
+                <small id="helpId" class="form-text text-muted ">เพศ</small>
+                <select class="form-control" name="" id="">
+                    <option>ชาย</option>
+                    <option>หญิง</option>
+                </select>
+            </div>
+            <div class="col-6 form-group w-100 p-0 m-0">
+
+                <small id="helpId" class="form-text text-muted">asdasd</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-6 form-group w-100 p-0 pr-3 m-0">
+
+                <small id="helpId" class="form-text text-muted" id="datepicker">วัน เดือน ปีเกิด</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-6 form-group w-100 p-0 m-0">
+
+                <small id="helpId" class="form-text text-muted">นามสกุล</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <!-- <button type="button" class="btn btn-primary" id="click"></button> -->
+
+        </div>
+
+    </div>
+</div>
