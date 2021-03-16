d24a72cae3db2a98bfc27bba4609be37cc275d70
DESKTOP-FO9RCJD\FANG <fang.live-@msn.com>
2021-03-16 17:42:46 +0700

    แบบฟอร์ม

--- component/applicantform.php
+++ component/applicantform.php
@@ -1,14 +1,14 @@
 <div class="col-12">
     <h1 class=" col-12 text-center pt-3 ">ใบสมัครงาน</h1>
     <div class=" col-12 ">
-        <div class=" col-12 d-flex justify-content-between flex-wrap">
+        <p class="p-0 m-0">ชื่อ-นามสกุล</p>
+        <div class=" col-12 p-0 m-0 d-flex justify-content-between flex-wrap">
             <div class="col-6 form-group w-100  p-0 pr-3  m-0">
-                <label for=""></label>
                 <small id="helpId" class="form-text text-muted">ชื่อ</small>
                 <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
             </div>
             <div class="col-6 form-group w-100 p-0 m-0">
-                <label for=""></label>
+
                 <small id="helpId" class="form-text text-muted">นามสกุล</small>
                 <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
             </div>
@@ -19,22 +19,131 @@
                     <option>หญิง</option>
                 </select>
             </div>
-            <div class="col-6 form-group w-100 p-0 m-0">
-
-                <small id="helpId" class="form-text text-muted">asdasd</small>
-                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-            </div>
-            <div class="col-6 form-group w-100 p-0 pr-3 m-0">
+            <div class="col-4 form-group w-100 p-0 pr-3  m-0">
 
                 <small id="helpId" class="form-text text-muted" id="datepicker">วัน เดือน ปีเกิด</small>
-                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                <input type="date" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
             </div>
-            <div class="col-6 form-group w-100 p-0 m-0">
+            <div class="col-2 form-group w-100 p-0  m-0">
 
-                <small id="helpId" class="form-text text-muted">นามสกุล</small>
+                <small id="helpId" class="form-text text-muted">อายุ</small>
+                <input type="text" readonly class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+
+            <div class="col-3 form-group w-100 p-0 pr-3 m-0">
+
+                <small id="helpId" class="form-text text-muted">ส่วนสูง</small>
                 <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
             </div>
-            <!-- <button type="button" class="btn btn-primary" id="click"></button> -->
+
+            <div class="col-3 form-group w-100 p-0 pr-3 m-0">
+
+                <small id="helpId" class="form-text text-muted">น้ำหนัก</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-6 form-group w-100 p-0  m-0">
+
+                <small id="helpId" class="form-text text-muted">เลขบัตรประจำตัวประชาชน</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+
+            <div class="col-6 form-group w-100 p-0 pr-3 m-0">
+
+                <small id="helpId" class="form-text text-muted">เบอร์ติดต่อ</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-6 form-group w-100 p-0  m-0">
+
+                <small id="helpId" class="form-text text-muted">เบอร์ติดต่อ</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+
+        </div>
+        <p class="p-0 pt-3 m-0">การศึกษา</p>
+        <div class=" col-12 p-0 m-0 d-flex justify-content-between flex-wrap">
+
+            <div class=" col-6 form-group p-0   pr-3 m-0">
+                <small id="helpId" class="form-text text-muted ">ระดับการศึกษา</small>
+                <select class="form-control" name="" id="">
+
+                    <option>ประถมศึกษาตอนต้น</option>
+                    <option>ประถมศึกษาตอนปลาย</option>
+                    <option> มัธยมศึกษาตอนต้น</option>
+                    <option>มัธยมศึกษาตอนปลายหรือเทียบเท่า</option>
+                    <option> อาชีวศึกษาหรือวิชาชีพ</option>
+                    <option> ปริญญาตรีหรือเทียบเท่า</option>
+                    <option> สูงกว่าปริญญาตรี</option>
+
+                </select>
+            </div>
+
+            <div class="col-6 form-group w-100 p-0  m-0">
+
+                <small id="helpId" class="form-text text-muted">ชื่อสถาศึกษา</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+
+            <div class="col-6 form-group w-100 p-0 pr-3 m-0">
+
+                <small id="helpId" class="form-text text-muted">สาขาที่จบ</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-3 form-group w-100 p-0 pr-3  m-0">
+
+                <small id="helpId" class="form-text text-muted">เกรดเฉลี่ย</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+
+            <div class="col-3 form-group w-100 p-0  m-0">
+
+                <small id="helpId" class="form-text text-muted">ปี พ.ศ ที่จบ</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+
+
+        </div>
+        <p class="p-0 pt-3 m-0">ความสามารถพิเศษ</p>
+        <div class=" col-12 p-0 m-0 d-flex justify-content-between flex-wrap">
+
+            <div class="col-12 form-group w-100 p-0   m-0">
+
+                <small id="helpId" class="form-text text-muted">ทางเชื่อม</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="col-12 form-group w-100 p-0  m-0">
+
+                <small id="helpId" class="form-text text-muted">ทางขับแมคโคร</small>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+            </div>
+        </div>
+        <p class="p-0 pt-3 m-0">ข้าเจ้า</p>
+        <div class=" col-12 p-0 m-0 d-flex justify-content-between flex-wrap">
+
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    เคยเป็นหนักงานของบริษัทมาก่อน (พ้นสภาพเมื่อปี พ.ศ _______ )
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+
+                <label class="form-check-label" for="flexRadioDefault2">
+                    ไม่เคยเป็นพนักงานของบริษัทมาก่อน
+                </label>
+
+            </div>
+        </div>
+        <p class="p-0 pt-3 m-0">โปรดเตรียมเอกสารมาในวันสัมภาษณ์งาน ดังนี้</p>
+        <div class=" col-12 p-0 m-0 d-flex justify-content-between flex-wrap">
+            <ol>
+                <li>รูปถ่ายขนาด 1 นิ้ว จำนวน 2 ใบ</li>
+                <li>สำเนาบัตรประชาชน</li>
+                <li>สำเนาทะเบียนบ้าน</li>
+                <li>สำเนาวุฒิการศึกษา (วุฒิที่ใช้สมัครงาน)</li>
+                <li>สำเนาหนังสือรับรองการทำงาน/ใบผ่านงาน (ถ้ามี)</li>
+                <li>สำเนาใบผ่านการเรียน ร.ด (ถ้ามี)</li>
+            </ol>
 
         </div>
 
